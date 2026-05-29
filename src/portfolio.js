24cd6723439582252c16bc480a2c698bc4e9ff85
Tinashe Kwangwari <tinashekwangwari@Tinashes-MacBook-Air.local>
2026-05-29 12:50:52 +0100

    Updated portfolio images

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,16 +1,30 @@
 const portfolio = [
   {
+    title: "Event Printing",
+    image: "/images/event1.jpg",
+  },
+  {
+    title: "Custom Event Apparel",
+    image: "/images/event2.jpg",
+  },
+  {
+    title: "Branded Event Wear",
+    image: "/images/event3.jpg",
+  },
+  {
+    title: "Professional Workwear",
     image: "/images/work1.jpg",
-    title: "Gym Workwear",
   },
   {
+    title: "Printed Uniforms",
     image: "/images/work2.jpg",
-    title: "Business Uniforms",
   },
   {
+    title: "Custom Business Clothing",
     image: "/images/work3.jpg",
-    title: "Event T-Shirts",
   },
 ];
+
+export default portfolio;
 
 export default portfolio;
